40c4da664f1c40f93a9ac18e6e6945e3dd695cc1
Adam White <adam.nathaniel.white@gmail.com>
2016-01-31 10:26:20 -0500

    Add some css and javascript functionality.

--- PROJECT/public/js/javascript.js
+++ PROJECT/public/js/javascript.js
@@ -1,4 +1,6 @@
 (function(){
+	/*
+	//delete this below
 	document.getElementById("button").addEventListener("click", function(){
 		first = parseInt(document.getElementById("first").value);
 		second = parseInt(document.getElementById("second").value);
@@ -6,5 +8,16 @@
 		sum = first + second;
 		
 		document.getElementById("result").innerHTML = sum;
-	});
+	});*/
+	
+	//document.getElementById("debug").innerHTML = "hey!";
+	
+	document.getElementById("refinanceCheckbox").addEventListener("click", function(){
+		if(document.getElementById("refinanceCheckbox").checked) {
+			document.getElementById("refinanceOptionDiv").hidden = false;
+			//document.getElementById("debug").innerHTML = "hey!";
+		} else {
+			document.getElementById("refinanceOptionDiv").hidden = true;
+		}
+	}, false);
 }());
